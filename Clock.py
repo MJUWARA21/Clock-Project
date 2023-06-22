36eeb6bf769d18a3eb941132d1b016ec193587d4
Mbemba Juwara <Mbemba700@gmail.com>
2023-06-22 13:09:16 +0100

    Clock-Project

--- Clock.py
+++ Clock.py
@@ -1,16 +1,17 @@
-from tkinker import *
-From tkinker.ttk import *
-
-from time import strtime
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
 
 root = Tk()
-root.title("clock")
-def time():
-    string = strftime('%H:%M:%S %P')
-    label.config(text=string)
-    label.after(1000,time)
+root.title("Clock")
 
-label = Label(root, front=("ds-digital",80) background = "cyan", foreground = "black")
-label.pack(anchor= 'center')
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+label = Label(root, font=("ds-digital", 80), background="cyan", foreground="black")
+label.pack(anchor='center')
+
 time()
-mainloop()
+root.mainloop()
